bfe341b8db04650f8b21b8a146f105b82634f2fa
Daniel <ada10dlu@student.lu.se>
2015-03-26 09:56:31 +0100

    Added implent 2

---
+++ phproot/project/production.php
@@ -0,0 +1,40 @@
+<?php
+	require_once('database.inc.php');
+	
+	session_start();
+	$db = $_SESSION['db'];
+	$userId = $_SESSION['userId'];
+	$db->openConnection();
+	
+//	$movieNames = $db->getMovieNames();
+//	$db->closeConnection();
+?>
+
+<html>
+<head><title>Krusty Kookies Sweden AB</title><head>
+<body><h1>Production</h1>
+	<h1>Blocking</h1>
+	<h1>Search</h1>
+	Current user: <?php print $userId ?>
+	<p>
+	Movies showing:
+	<p>
+	<form method=post action="booking2.php">
+		<select name="movieName" size=10>
+		<?php
+			$first = true;
+			foreach ($movieNames as $name) {
+				if ($first) {
+					print "<option selected>";
+					$first = false;
+				} else {
+					print "<option>";
+				}
+				print $name;
+			}
+		?>
+		</select>		
+		<input type=submit value="Select movie">
+	</form>
+</body>
+</html>
